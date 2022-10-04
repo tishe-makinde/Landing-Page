45a081da5b9e1dad389b0441c6e2b10e2f515c03
tishe <tishdmakinde@gmail.com>
2022-10-04 19:38:37 +0100

    25291368d9cfd917be051da247f4b8a82b3b5166

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import AboutMe from "./components/Aboutme";
+
+
+// var clicked = false
+
+// $(".description").mouseover(function() {
+//   if (clicked === false) {
+//     $(".description").addClass("hover-effect")
+//   }
+// });
+// $(".description").mouseout(function() {
+//   $(".description").removeClass("hover-effect")
+// });
+
+// $(".description").click(function() {
+//   clicked = true
+//   $(".about").text('"I am a full stack developer based in Oxford, UK. I am very proficient in the MERN tech stack with more than enough versality to adapt to other languages and libraries you may need."')
+// })
+
+ReactDOM.render( <h1>hi</h1> , document.getElementById("me"));
+
